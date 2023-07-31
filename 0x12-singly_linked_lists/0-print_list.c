c5038d9bd9b07913c9896b010660e0beabdc2885
Adeelizabeth <honarres.collections@gmail.com>
2023-07-30 18:46:48 -0700

    singly linked

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -15,18 +15,18 @@ int count = 0;
 
 
 while (h)
-   {
-	if (h->str == NULL)
-   {
- printf("[0] (nil)\n");
-   }
-       else
-   {
+{
+if (h->str == NULL)
+{
+printf("[0] (nil)\n");
+}
+else
+{
 printf("[%d] %s\n", h->len, h->str);
-    }
+}
 count++;
 = h->next;
-   }
-    return (count);
+}
+return (count);
 }
 
